fab15db8b1951165987063a38f08cca673dc0f89
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-02-09 23:58:44 +0100

    upload user class

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ 
+    Inherits from BaseModel
+    Args:
+    email - string
+    password - string
+    first_name - string
+    last_name - string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
